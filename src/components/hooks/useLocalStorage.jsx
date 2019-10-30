7316611673ae07761233cbc13e7a04391979f595
Nicole Adams <nicoleadams.boston@gmail.com>
2019-10-30 16:10:10 -0400

    useLocalStorage correction per README instrucitons

--- src/components/hooks/useLocalStorage.jsx
+++ src/components/hooks/useLocalStorage.jsx
@@ -3,10 +3,7 @@ import React from 'react';
 function useLocalstorage (key, initialValue)  {
     
     const [storedValue, setStoredValue] = useState(()=> {
-        if (window.localStorage.getItem(key)){
-            return JSON.parse(window.localStorage.getItem(key));
-        }
-        window.localStorage.setItem(JSON.stringify(initialValue))
-        return initialValue
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
     });
 }
